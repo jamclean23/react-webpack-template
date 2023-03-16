87aae6bd0fd3fbbf97a73ba6509fe0a4697fadfb
jamclean23 <109743864+jamclean23@users.noreply.github.com>
2023-03-16 14:55:34 -0400

    Remove outdated test function from test.test.js

--- src/__tests__/test.test.js
+++ src/__tests__/test.test.js
@@ -1,11 +1,5 @@
 // Test file to see if jest is working
 
-import { printSomething } from "../index/function";
-
 test('Jest is running a test',  () => {
     expect(1).toBe(1);
-});
-
-test('Jest is importing a function and returning the correct result', () => {
-    expect(printSomething()).toBe('something');
 });
